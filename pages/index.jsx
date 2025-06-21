4ac7c78b1a15f99edcbae79ed1560ccbd71330cb
Moumita Maity <moumitamaity150@gmail.com>
2025-06-21 21:11:49 +0530

    changes done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,7 @@
 import FirstSection from '@/components/FirstSection'
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
 import SecondSection from '@/components/secondSection'
 import StatsSection from '@/components/StatsSection'
 import TestimonialsSection from '@/components/TestimonialsSection'
-import TouchForm from '@/components/TouchForm'
 import WhoWeAreSection from '@/components/WhoWeAreSection'
 import WhyChooseUsSection from '@/components/WhyChooseUsSection'
 
